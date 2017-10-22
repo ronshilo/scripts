8be2fa5fc4d53b387da2b2eb51b0b986425436f1
ronsh <ronshilo@gmail.com>
2017-10-22 23:49:20 +0300

    open firefox and not chrome one more instance test
    open firefox and not chrome
    one more instance test

--- run_tunnel_aws_server.sh
+++ run_tunnel_aws_server.sh
@@ -9,12 +9,25 @@ while [ $code != "running" ]; do
 	echo the AWS instance is $code
 	sleep 5
 done
+
+
+code=$(aws ec2 describe-instance-status | python -c "import sys, json; print json.load(sys.stdin)['InstanceStatuses'][0]['SystemStatus']['Status']")
+while [ $code != "ok" ]; do
+	code=$(aws ec2 describe-instance-status | python -c "import sys, json; print json.load(sys.stdin)['InstanceStatuses'][0]['SystemStatus']['Status']")        
+	echo the AWS instance is $code
+        sleep 5
+done
+
+
+
 echo start ssh tunnal
 ssh -i ~/.ssh/ronnewvm.pem -o StrictHostKeyChecking=no -D 8123 -C -q -N -o ConnectTimeout=120 ubuntu@$the_ip &
 echo tunnal open
 echo set proxy config
 sudo networksetup -setsocksfirewallproxy Wi-Fi 127.0.0.1 8123 ;
-/usr/bin/open -W -a "/Applications/Google Chrome.app" "https://www.netflix.com/browse"
+sleep 5
+#/usr/bin/open -W -a "/Applications/Google Chrome.app" "https://www.netflix.com/browse"
+/usr/bin/open -W -a "/Applications/Firefox.app" "https://www.netflix.com/browse"
 echo app open
 pkill ssh
 sudo networksetup -setsocksfirewallproxystate Wi-Fi off
